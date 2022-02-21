cd253b45ccc9ccceb2a1cc529b1f38ee79a3b558
Ryuji.Chida <ryuji.chida@mixi.co.jp>
2022-02-21 09:09:43 +0900

    add import

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
   @default_count 4
 
   @moduledoc """
@@ -51,11 +52,26 @@ defmodule Issues.CLI do
   def process([user, project, _count]) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
   end
 
-  def decode_response([:ok, body]),  do: body
+  def decode_response([:ok, body]), do: body
+
   def decode_response([:error, error]) do
-    IO.puts "Error fetching from Github: #{error["message"]}"
+    IO.puts("Error fetching from Github: #{error["message"]}")
     System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 end
