b4a3d782898ff2b6aeb4cac2e6a62b9d6ec2f29c
Ryuji.Chida <ryuji.chida@mixi.co.jp>
2022-02-23 09:23:23 +0900

    rename run -> main

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,7 @@ defmodule Issues.CLI do
   the various functions that end up generationg a
   table of the last _n_ issues in a github project
   """
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_argv()
     |> process()
